284a30125c5b8fa0ab48845a68bb0f4648dd8984
David <114953208+davida876@users.noreply.github.com>
2022-11-03 18:44:33 -0700

    Update actualizarRegistro.php

--- parcial2/PracticaWebDatos/actualizarRegistro.php
+++ parcial2/PracticaWebDatos/actualizarRegistro.php
@@ -12,7 +12,7 @@
     <?php
         include 'conexion.php';
         $id = $_GET["id"];
-        $sql = "SELECT * FROM usuarios WHERE id=" . $id;
+        $sql = "SELECT * FROM usuariosBlockBuster WHERE id=" . $id;
         $resultado = $conexion->query($sql);
         $registro = $resultado->fetch_assoc();
     ?>
@@ -25,36 +25,29 @@
                 <form method="post" action="guardarRegistro.php">
                     <input name="id" type="hidden" value="<?php echo $registro["id"]; ?>">
                     <div class="form-group">
-                        <label for="">Nombre:</label>
-                        <input value="<?php echo $registro["nombre"]; ?>" type="text" class="form-control" name="nombre" placeholder="Teclea el nombre">
+                        <label for="">Nombre del producto:</label>
+                        <input value="<?php echo $registro["nombre_producto"]; ?>" type="text" class="form-control" name="nombre_producto" placeholder="Teclea el nombre del producto">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Edad:</label>
-                        <input value="<?php echo $registro["edad"]; ?>" type="number" class="form-control" name="edad" placeholder="Teclea la edad">
+                        <label for="">Tipo del producto:</label>
+                        <input value="<?php echo $registro["tipo_producto"]; ?>" type="text" class="form-control" name="tipo_producto" placeholder="Teclea el tipo del producto">
                     </div>
 
                     <div class="form-group">
-                    <?php
-                        if($registro["sexo"]){
-                            echo "<input type='radio' name='sexo' value='1' checked> Masculino <br>";
-                            echo "<input type='radio' name='sexo' value='0'> Femenino <br>";
-                        } else {
-                            echo "<input type='radio' name='sexo' value='1'> Masculino <br>";
-                            echo "<input type='radio' name='sexo' value='0' checked> Femenino <br>";
-                        }
-                    ?>
+                        <label for="">Nombre del cliente:</label>
+                        <input value="<?php echo $registro["nombre_cliente"]; ?>" type="text" class="form-control" name="nombre_cliente" placeholder="Teclea el nombre del cliente">
                     </div>
 
                     <div class="form-group">
-                        <label for="">Domicilio:</label>
+                        <label for="">Domicilio del cliente:</label>
                         <textarea name="domicilio" id="" cols="30" rows="10" class="form-control"><?php echo $registro["domicilio"] ?>
                         </textarea>
                     </div>
 
                     <div class="form-group">
-                        <label for="">Fecha de nacimiento:</label>
-                        <input value="<?php echo $registro["fecha_nacimiento"]; ?>" type="date" class="form-control" name="fecha_nacimiento">
+                        <label for="">Fecha de renta/compra:</label>
+                        <input value="<?php echo $registro["fecha_renta/compra"]; ?>" type="date" class="form-control" name="fecha_nacimiento">
                     </div>
                     <div>
                         <input type="submit" class="btn btn-primary" value="Registrar">
@@ -71,3 +64,4 @@
     <script src="js/bootstrap.js"></script>
 </body>
 </html>
+
