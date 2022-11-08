bc7335d6f8f7461114580d820901187d4fe1bec9
Sergio Guillermo Perez Perez <99852547+SergioGPerez@users.noreply.github.com>
2022-11-07 18:15:18 -0700

    Update guardarDatos.php

--- parcial2/PracticaWebDatos/guardarDatos.php
+++ parcial2/PracticaWebDatos/guardarDatos.php
@@ -8,7 +8,8 @@
     $domicilio = $_POST["domicilio"];
     $fecha_rentacompra = $_POST["fecha_rentacompra"];
 
-    $sql = "INSERT INTO usuariosBlockBuster (nombre_producto, tipo_producto, nombre_cliente, domicilio, fecha_rentacompra)". "VALUES ('".$nombreProducto."', ".$tipoProducto.", ".$nombreCliente.", '".$domicilio."', '".$fecha_rentacompra."')";
+    $sql = "INSERT INTO usuariosBlockBuster (nombre_producto, tipo_producto, nombre_cliente, domicilio, fecha_rentacompra)". 
+    "VALUES ('".$nombreProducto."', '".$tipoProducto."', '".$nombreCliente."', '".$domicilio."', '".$fecha_rentacompra."')";
 
     if($conexion->query($sql) === TRUE){
         echo "Registro guardado con Éxito <a href='consultarDatos.php'>Regresar</a>";
@@ -18,3 +19,4 @@
 
     $conexion->close();
 ?>
+
